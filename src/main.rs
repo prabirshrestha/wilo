c0e1a72e4c3e5ca189e7cf7f50329a92dc7a88ba
Prabir Shrestha <mail@prabir.me>
2020-05-17 05:18:40 +0000

    handle keys

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use anyhow::Result;
 use termwiz::{
     caps::Capabilities,
+    input::*,
     terminal::{buffered::BufferedTerminal, SystemTerminal, Terminal},
 };
 
 pub struct Editor {
     buf: BufferedTerminal<SystemTerminal>,
+    should_quit: bool,
 }
 
 impl Editor {
     pub fn new() -> Result<Self> {
         let buf = BufferedTerminal::new(SystemTerminal::new(Capabilities::new_from_env()?)?)?;
-        Ok(Self { buf })
+        Ok(Self {
+            buf,
+            should_quit: false,
+        })
     }
 
     pub fn run(&mut self) -> Result<()> {
@@ -19,9 +24,35 @@ impl Editor {
         self.buf.terminal().set_raw_mode()?;
         self.buf.flush()?;
 
-        self.buf.add_change("Hello world\n");
+        loop {
+            self.handle_keys()?;
+            if self.should_quit {
+                break;
+            }
+        }
+
         self.buf.flush()?;
 
+        Ok(())
+    }
+
+    fn handle_keys(&mut self) -> Result<()> {
+        match self.buf.terminal().poll_input(None) {
+            Ok(Some(input)) => match input {
+                InputEvent::Key(KeyEvent {
+                    key: KeyCode::Char('Q'),
+                    modifiers: Modifiers::CTRL,
+                }) => {
+                    self.should_quit = true;
+                }
+                _ => {}
+            },
+            Ok(None) => {}
+            Err(e) => {
+                println!("{:?}\r\n", e);
+                self.should_quit = true;
+            }
+        }
         Ok(())
     }
 }
