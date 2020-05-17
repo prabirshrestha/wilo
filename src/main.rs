ddfaee8b571285f7e40e0ab083d7e854271d8ebd
Prabir Shrestha <mail@prabir.me>
2020-05-17 05:05:53 +0000

    add termwiz

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use anyhow::Result;
+use termwiz::{
+    caps::Capabilities,
+    terminal::{buffered::BufferedTerminal, SystemTerminal},
+};
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let mut buf = BufferedTerminal::new(SystemTerminal::new(Capabilities::new_from_env()?)?)?;
+    buf.add_change("Hello world\n");
+    buf.flush()?;
     Ok(())
 }
